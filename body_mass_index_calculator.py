c945984c67cb281a47377e9a10856096a8652d6a
Chimjiokem <chimjiokemanya@gmail.com>
2024-11-25 01:55:17 -0800

    second commit

--- body_mass_index_calculator.py
+++ body_mass_index_calculator.py
@@ -1,12 +1,13 @@
 name = input('Enter your name: ')
+
 weight = int(input(f'Your  weight in kilograms is: '))
-if weight <= 0:
-    print('Error! Invalid value entered!')
-height = float(input(f'Your  height in meters is: '))
-if height <= 0:
+height = int(input(f'Your  height in metres is: '))
+
+if weight <= 0 or height <= 0:
     print('Error! Invalid value entered!')
 
 body_mass_index = weight/height**2
+
 if body_mass_index < 18.5:
     print(f'{name}, your body mass index is {body_mass_index:.2f}\nYou are underweight')
 elif body_mass_index >= 18.5 and body_mass_index < 25:
